14c6187a1d2b64c2c4dadf2692bf2bb9fd2a5c75
Flo3376 <Flo3376@users.noreply.github.com>
2020-05-17 10:03:11 +0200

    ajout des automatismes BM

--- Events/guildMemberUpdate.js
+++ Events/guildMemberUpdate.js
@@ -1,6 +1,6 @@
 module.exports = async(client,oldMember, newMember)=>{
 	// Si les rôles sont présents sur l'ancien objet membre mais plus sur le nouveau (c'est-à-dire que les rôles ont été supprimés)
-	const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+	/*const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 	if (removedRoles.size > 0)
 	{
 		let role_id= removedRoles.map(r => r.id)[0];
@@ -11,7 +11,7 @@ module.exports = async(client,oldMember, newMember)=>{
 		
 	}
 	console.log(`The roles ${removedRoles.map(r => r.id)} were removed from ${oldMember.displayName}.`);
-	
+	*/
 	// Si le ou les rôles sont présents sur le nouvel objet membre mais ne le sont pas sur l'ancien (c'est-à-dire que les rôles ont été ajoutés)
 	const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 	if (addedRoles.size > 0)
@@ -19,9 +19,19 @@ module.exports = async(client,oldMember, newMember)=>{
 		let role_id= addedRoles.map(r => r.id)[0];
 		if(role_id ===config.bm_id_role)
 		{
-			newMember.send("bienvenue")
+			//newMember.send("bienvenue")
+
+			//chargement du template (méthode provisoire car dangereuse avec l'utilisation de la méthode eval())
+			fs.readFile("./embed/to_bm.js", "utf-8", (err, data) => {
+				//interprétation du template
+				eval(data)
+				//envoie du message privée avec la réponse
+				newMember.send(my_embed)
+			});
+
+			
 		}
 	} 
-	console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+	console.log(`Le rôle ${addedRoles.map(r => r.name)} a été ajouté à ${oldMember.displayName}.`);
 
 };
